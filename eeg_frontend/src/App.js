89967d0456600965321b4dfbf9940b878a95011b
Viswadeep Manam <manam.viswadeep@gmail.com>
2023-03-11 02:22:04 -0800

    Connection Code.

--- eeg_frontend/src/App.js
+++ eeg_frontend/src/App.js
@@ -4,6 +4,7 @@ function App() {
     const startEmotiveClient = async () => {
         let client = new EmotiveClient('jywCIH1KljuJcJlVpqrYP1OhyOnT1hIpZfLKTEu7', 'EW9pQmSvuHIrTju192gJvmWID7kVumwNRFhUsajaSUAJpzTSKdxcGIMqKzjlvYIGBXkTJUiZ8EFylJ9PUZfxS9iVcv4aq3SJkuuPG1cYjdROBwjKCatAci4dVsvnGhOp');
         await client.init();
+        await client.connect();
     }
 
   return (
--- eeg_frontend/src/services/EmotiveClient.js
+++ eeg_frontend/src/services/EmotiveClient.js
@@ -1,8 +1,13 @@
 const CORTEX_URL = "wss://localhost:6868";
 
+//Request ID
 const REQUIRE_ACCESS_ID = 1;
 const AUTHORIZE_ID = 2;
 const QUERY_HEADSET_ID = 3;
+const CONTROL_DEVICE_ID = 4;
+
+//Warning Codes
+const CONTROL_DEVICE_CODES = [100, 101, 102, 104, 113];
 
 const createDeferredPromise = () => {
     let deferredResolve;
@@ -20,6 +25,8 @@ class EmotiveClient {
         this.clientSecret = clientSecret;
         this.requests = {};
 
+        this.isConnected = false;
+
         this.ws = new WebSocket(CORTEX_URL);
         this.ws.addEventListener('message', (event) => {
            this.onMessage(event.data);
@@ -31,7 +38,14 @@ class EmotiveClient {
         await this.openConnection();
         await this.requestAccess();
         await this.authorize();
-        console.log(this.cortexToken);
+    }
+
+    async connect() {
+        await this.queryHeadsets();
+        console.log(`HEADSET ID ${this.headsetId}`);
+        if(!this.isConnected) await this.connectHeadset();
+
+        console.log(`CONNECTED TO ${this.headsetId}`);
     }
 
     openConnection() {
@@ -89,10 +103,67 @@ class EmotiveClient {
         return p;
     }
 
+    queryHeadsets() {
+        let req = {
+            "id": QUERY_HEADSET_ID,
+            "jsonrpc": "2.0",
+            "method": "queryHeadsets"
+        }
+
+        let [resolve, reject, p] = createDeferredPromise();
+
+        const handler = (res) => {
+            this.headsetId = res["result"][0]["id"]; // assuming only 1 headset is being used
+            this.isConnected = res["result"][0]["status"] === "connected" ? true : false;
+            resolve();
+        }
+
+        this.requests[QUERY_HEADSET_ID] = handler;
+        this.ws.send(JSON.stringify(req));
+        return p;
+    }
+
+    connectHeadset() {
+        let req = {
+            "id": CONTROL_DEVICE_ID,
+            "jsonrpc": "2.0",
+            "method": "controlDevice",
+            "params": {"command": "connect", "headset": this.headsetId }
+        }
+
+        let [resolve, reject, p] = createDeferredPromise();
+
+        const handler = (res) => {
+            console.log(`CONTROL ${JSON.stringify(res)}`);
+            if("warning" in res) {
+                if(res["warning"]["code"] === 104) {
+                    this.isConnected = true;
+                    resolve();
+                }
+                else reject();
+            }
+        }
+
+        this.requests[CONTROL_DEVICE_ID] = handler;
+        this.ws.send(JSON.stringify(req));
+        return p;
+
+    }
+
     onMessage(data) {
+        console.log(data);
         let res = JSON.parse(data);
         if("id" in res) {
             this.requests[res['id']](res);
+        }
+        else if("warning" in res) {
+            this.handleWarnings(res)
+        }
+    }
+
+    handleWarnings(res) {
+        if(CONTROL_DEVICE_CODES.includes(res["warning"]["code"])) {
+            this.requests[res[CONTROL_DEVICE_ID]](res);
         }
     }
 
