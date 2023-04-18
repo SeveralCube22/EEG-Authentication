42731cf0002774450130c280e0d27c9e9332057d
Viswadeep Manam <manam.viswadeep@gmail.com>
2023-04-17 22:32:56 -0700

    Progress on preprocessing.

---
+++ eeg_preprocessing/src/handler.py
@@ -0,0 +1,15 @@
+import preprocess
+
+def handler(event, context):
+    data = event["data"]
+
+    test_df = preprocess.tester(data)
+    df, ica_objects = preprocess.apply_ica(test_df)
+    df = preprocess.window_data(df)
+    psd_list = preprocess.extract_features(df.copy())
+    selected_columns = ['0_beta', '2_gamma', '3_gamma', '6_gamma', '7_alpha', '9_beta', '9_gamma', '10_gamma', '11_gamma', '12_gamma']
+
+    print(psd_list)
+    selected_df = psd_list[selected_columns]
+
+    return selected_df
