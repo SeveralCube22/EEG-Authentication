2b36b415ba9ca45c58b94f150c4e8dca05b67999
Viswadeep Manam <manam.viswadeep@gmail.com>
2023-04-20 21:58:11 -0700

    Controller

---
+++ eeg_backend/src/main/java/com/vmanam/eeg_backend/Controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.vmanam.eeg_backend.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String getHome() {
+        return "index.html";
+    }
+}
